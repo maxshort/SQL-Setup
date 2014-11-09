fb213109de23c554d7fd59ef6df7b88ce5051bd2
maxshort <maxshort@users.noreply.github.com>
2014-11-08 19:24:33 -0600

    "Final version of database"
    Contains both the visual and the script to create the database. The
    script is specific to a database called shortjm (my username) because I
    did not have the rights to create a new database.

---
+++ dbsetup.sql
@@ -0,0 +1,71 @@
+USE shortjm;
+
+CREATE TABLE Hotels
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(500) NOT NULL,
+	picture_location VARCHAR(500),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Restaurants
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	restaurant_name VARCHAR(500),	
+	hotel INT NOT NULL,
+	Button_Image VARCHAR(500),
+	Advertising_Image VARCHAR(500),
+	Description VARCHAR(1000),
+	FOREIGN KEY (hotel) REFERENCES Hotels(id),
+	PRIMARY KEY(id)
+);
+/*These were intned for the slide show.*/
+CREATE TABLE RestaurantPictures
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	restaurant INT,
+	picture_location VARCHAR(500),
+	FOREIGN KEY (restaurant) REFERENCES Restaurants(id),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE Categories
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(500),
+	restaurant INT,
+	FOREIGN KEY(restaurant) REFERENCES Restaurants(id),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE MenuItems
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(500),
+	category INT,
+	description VARCHAR(2000),
+	picture_file VARCHAR(500),
+	price NUMERIC(6,2),
+	review_image_location VARCHAR(500),
+	FOREIGN KEY (category) REFERENCES Categories(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Ingredients
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(2000),
+	in_stock BIT,
+	restaurant INT,
+	FOREIGN KEY (restaurant) REFERENCES Restaurants(id),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IngredientsInItems
+(
+	menu_item INT,
+	ingredient INT,
+	essential BIT,
+	FOREIGN KEY (menu_item) REFERENCES MenuItems(id),
+	FOREIGN KEY (ingredient) REFERENCES Ingredients(id)
+);
